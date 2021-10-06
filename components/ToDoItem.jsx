90ad9af66d63923877ff2d171c06e970f28e50ed
Samir Schabel <schabel.samir@gmail.com>
2021-10-06 22:33:08 +0200

    changes

--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
 import "./ToDoItem.css";
+import herberttwo from "../src/media/dont-make-me-beg.mp3";
+import herbertone from "../src/media/herbert_ymca.mp3";
 
 export const ToDoItem = ({ name }) => {
+  r;
   const [isDone, setIsDone] = useState(false);
   const doneClick = () => {
     setIsDone(!isDone);
+    isDone ? new Audio(herbertone).play() : new Audio(herberttwo).play();
   };
 
   const doneToggle = isDone ? "todo done" : "todo";
