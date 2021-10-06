f53b4896245a7c52356c0e33abbdab813834de6f
Samir Schabel <schabel.samir@gmail.com>
2021-10-06 09:41:10 +0200

    add ToDoList components

---
+++ components/TodoList.jsx
@@ -0,0 +1,12 @@
+import { ToDoItem } from "./ToDoItem";
+
+export const ToDoList = () => {
+  return (
+    <ul>
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+      <ToDoItem />
+    </ul>
+  );
+};
