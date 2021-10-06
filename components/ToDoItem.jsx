2a58708ae0db22f7e5212433823d564c2a1fabc0
Samir Schabel <schabel.samir@gmail.com>
2021-10-06 13:52:38 +0200

    change

--- components/ToDoItem.jsx
+++ components/ToDoItem.jsx
@@ -1,3 +1,15 @@
-export const ToDoItem = (props) => {
-  return <li>{props.name}</li>;
+import "./";
+
+export const ToDoItem = ({ name }) => {
+  const [isDone, setIsDone] = useState(false);
+  const doneClick = () => {
+    setIsDone(!isDone);
+  };
+
+  const doneToggle = isDone ? "todo done" : "todo";
+  return (
+    <li className={doneToggle} onClick={doneClick}>
+      {name}
+    </li>
+  );
 };
